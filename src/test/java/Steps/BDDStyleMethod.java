36478da37c130947b4eaf15cb54753a2854a256b
Aniket <aniketneogy19@gmail.com>
2020-09-02 00:33:45 +0530

    Implemented query and path parameters for Rest Assured BDD DSL

--- src/test/java/Steps/BDDStyleMethod.java
+++ src/test/java/Steps/BDDStyleMethod.java
@@ -7,27 +7,68 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class BDDStyleMethod {
-    public static void simpleGetPostsMethod(String url,String resource,String postNumber,String author){
+    public static void simpleGetPostsMethod(String url, String resource, String postNumber, String author) {
         RestAssured.baseURI = url;
         given().
                 contentType(ContentType.JSON).
                 when().
-                    get(resource+"/"+postNumber).
+                get(resource + "/" + postNumber).
                 then().
-                    body("author",is (author));// "is()" method is from hamcrest matchers library to check the equal condition
+                body("author", is(author));// "is()" method is from hamcrest matchers library to check the equal condition
 
     }
 
-    public static void performContainsCollection(String url,String resource,String authorList[]){
+    public static void performContainsCollection(String url, String resource, String authorList[]) {
         RestAssured.baseURI = url;
         given().
                 contentType(ContentType.JSON).
                 when().
-                get(resource+"/").
+                get(resource).
                 then().
-                    body("author",containsInAnyOrder(authorList)). // this will pass as the order is neglected only the values are considered
-                    body("author",containsInRelativeOrder(authorList)). //relative order will fail as we are passing authorlist as ["Shalini", "Aniket"] but from API we get [Aniket, Shalini]
-                    statusCode(200);
+                body("author", containsInAnyOrder(authorList)). // this will pass as the order is neglected only the values are considered
+                //body("author", containsInRelativeOrder(authorList)). //relative order will fail as we are passing authorlist as ["Shalini", "Aniket"] but from API we get [Aniket, Shalini]
+                statusCode(200);
+
+    }
+
+    public static void performPathParameter(String url, String resource, String postNumber, String author) {
+        RestAssured.baseURI = url;
+        given().
+                contentType(ContentType.JSON).
+                with().
+                pathParam("id", postNumber).
+                when().
+                get(resource + "/{id}").
+                then().
+                body("author", containsString(author)).
+                statusCode(200);
+
+    }
+
+    public static void performQueryParameter(String url, String resource, String postNumber, String author) {
+        RestAssured.baseURI = url;
+        given().
+                contentType(ContentType.JSON).
+                queryParam("id", postNumber).
+                when().
+                get(resource).
+                then().
+                body("author", hasItem(author)).
+                statusCode(200);
+
+    }
+
+    public static void performQueryParameterWithMultipleValues(String url, String resource, String firstPostNumber, String secondPostNumber, String size) {
+        RestAssured.baseURI = url;
+        given().
+                contentType(ContentType.JSON).
+                queryParam("id", firstPostNumber).
+                queryParam("id",secondPostNumber).
+                when().
+                get(resource).
+                then().
+                body("author", hasSize(Integer.parseInt(size))).
+                statusCode(200);
 
     }
 }
--- src/test/java/Steps/MyStepdefs.java
+++ src/test/java/Steps/MyStepdefs.java
@@ -8,13 +8,32 @@ public class MyStepdefs {
     public void iNeedToSetTheBaseUrlToAndDoGETOperationForResourceAndPostNumberAndCheckAuthorNameToBe
             (String url, String resource, String postNumber, String author) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        BDDStyleMethod.simpleGetPostsMethod(url,resource,postNumber,author);
+        BDDStyleMethod.simpleGetPostsMethod(url, resource, postNumber, author);
     }
 
     @Given("^I need to set the BaseUrl to \"([^\"]*)\" and do GET operation for resource \"([^\"]*)\" and verify authorList$")
     public void iNeedToSetTheBaseUrlToAndDoGETOperationForResourceAndVerifyAuthorList(String url, String resource) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        String authorList[]={"Shalini","Aniket"};
-        BDDStyleMethod.performContainsCollection(url,resource,authorList);
+        String authorList[] = {"Shalini", "Aniket"};
+        BDDStyleMethod.performContainsCollection(url, resource, authorList);
+    }
+
+
+    @Given("^I need to set the BaseUrl to \"([^\"]*)\" and do GET operation for resource \"([^\"]*)\" using the path parameter value of \"([^\"]*)\" and validate it to be \"([^\"]*)\"$")
+    public void iNeedToSetTheBaseUrlToAndDoGETOperationForResourceUsingThePathParameterValueOfAndValidateItToBe(String url, String resource, String postNumber, String author) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        BDDStyleMethod.performPathParameter(url, resource, postNumber, author);
+    }
+
+    @Given("^I need to set the BaseUrl to \"([^\"]*)\" and do GET operation for resource \"([^\"]*)\" using the query parameter value of \"([^\"]*)\" and validate it to be \"([^\"]*)\"$")
+    public void iNeedToSetTheBaseUrlToAndDoGETOperationForResourceUsingTheQueryParameterValueOfAndValidateItToBe(String url, String resource, String postNumber, String author) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        BDDStyleMethod.performQueryParameter(url, resource, postNumber, author);
+    }
+
+    @Given("^I need to set the BaseUrl to \"([^\"]*)\" and do GET operation for resource \"([^\"]*)\" using the query parameter value of \"([^\"]*)\" & \"([^\"]*)\" and validate the size equal to \"([^\"]*)\"$")
+    public void iNeedToSetTheBaseUrlToAndDoGETOperationForResourceUsingTheQueryParameterValueOfAndValidateTheSizeEqualTo(String url, String resource, String firstPostNumber, String secondPostNumber, String size) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        BDDStyleMethod.performQueryParameterWithMultipleValues(url, resource, firstPostNumber, secondPostNumber, size);
     }
 }
